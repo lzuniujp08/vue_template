4d83947d92ef2a8cd6fa187d14b9ca5e2293ff8a
lzugis <1004740957@qq.com>
2018-03-30 11:13:14 +0800

    init

--- src/pages/chart.vue
+++ src/pages/chart.vue
@@ -17,47 +17,231 @@
         },
         mounted: function () {
             const option = {
-                title: {
-                    text: 'ECharts 入门示例'
+                "tooltip": {"trigger": "axis"},
+                "legend": {"data": ["温度", "风速风向", "湿度", "降水量"], "x": "right"},
+                "grid": {"x": 80, "y": 80, "x2": 90, "y2": 60},
+                "dataZoom": [{"type": "inside", "realtime": true, "start": 0, "end": 100}, {
+                    "show": true,
+                    "realtime": true,
+                    "start": 0,
+                    "end": 100
+                }],
+                "xAxis": {
+                    "type": "category",
+                    "boundaryGap": false,
+                    "data": ["20180330,00:00", "20180330,00:30", "20180330,01:00", "20180330,01:30", "20180330,02:00", "20180330,02:30", "20180330,03:00", "20180330,03:30", "20180330,04:00", "20180330,04:30", "20180330,05:00", "20180330,05:30", "20180330,06:00", "20180330,06:30", "20180330,07:00", "20180330,07:30", "20180330,08:00", "20180330,08:30", "20180330,09:00", "20180330,09:30", "20180330,10:00"],
+                    "axisLabel": {},
+                    "axisLine": {"onZero": false}
                 },
-                tooltip: {},
-                xAxis: [
+                "visualMap": [
                     {
-                        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子',]
+                        "show": false,
+                        "seriesIndex": 0,
+                        "pieces": [{"gt": -30, "lte": 35, "color": "#ca6924"}],
+                        "outOfRange": {"color": "#f00"}
                     },{
-                        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子',]
+                        "show": false,
+                        "seriesIndex": 3,
+                        "pieces": [{"gt": 0, "lte": 50, "color": "#675bba"}],
+                        "outOfRange": {"color": "#f00"}
                     }
                 ],
-                yAxis: {},
-                series: [{
-                    xAxisIndex: 0,
-                    name: '销量',
-                    type: 'line',
-                    smooth:true,   //关键点，为true是不支持虚线的，实线就用true
-                    itemStyle:{
-                        normal:{
-                            lineStyle:{
-                                width:2,
-                                type:'solid'  //'dotted'虚线 'solid'实线
-                            }
-                        }
+                "yAxis": [
+                    {
+                        "type": "value",
+                        "name": "温度\r\n(°C)",
+                        "offset": 40,
+                        "min": -15,
+                        "max": 40,
+                        "position": "right",
+                        "interval": 5,
+                        "axisLabel": {"formatter": "{value}"},
+                        "axisLine": {"lineStyle": {"color": "#ca6924"}},
+                        "splitLine": {"lineStyle": {"color": ["#fff"], "width": 10, "type": "solid"}}
+                    }, {
+                        "type": "value",
+                        "name": "风速\r\n(m/s)",
+                        "offset": 12,
+                        "min": 0,
+                        "max": 20,
+                        "position": "left",
+                        "interval": 4,
+                        "axisLabel": {"formatter": "{value} "},
+                        "axisLine": {"lineStyle": {"color": "#4c8dae"}},
+                        "splitLine": {"lineStyle": {"color": ["#fff"], "width": 10, "type": "solid"}}
+                    }, {
+                        "type": "value",
+                        "name": "降水量\r\n(mm)",
+                        "offset": 6,
+                        "min": 0,
+                        "max": 20,
+                        "interval": 2,
+                        "axisLabel": {"formatter": "{value}"},
+                        "axisLine": {"lineStyle": {"color": "#065279"}},
+                        "splitLine": {"lineStyle": {"color": ["#f5f5f5"], "width": 1, "type": "solid"}}
+                    }, {
+                        "type": "value",
+                        "name": "湿度\r\n(%)",
+                        "min": 0,
+                        "max": 60,
+                        "position": "left",
+                        "offset": 45,
+                        "interval": 10,
+                        "axisLabel": {"formatter": "{value} "},
+                        "axisLine": {"lineStyle": {"color": "#675bba"}},
+                        "splitLine": {"lineStyle": {"color": ["#fff"], "width": 10, "type": "solid"}}
+                    }
+                ],
+                "series": [{
+                    "name": "温度",
+                    "yAxisIndex": 0,
+                    "type": "line",
+                    "itemStyle": {"normal": {"color": "#ca6924"}},
+                    "data": [-8.77, -8.99, -9.36, -9.14, -9.57, -9.57, -9.81, -9.98, -10.47, -10.42, -11.02, -11.27, -11.54, -11.62, -12.11, -12.18, -12.24, -12.05, -11.65, -11.24, -10.63]
+                },
+                    {
+                        "name": "风速风向",
+                        "yAxisIndex": 1,
+                        "type": "line",
+                        "itemStyle": {"normal": {"color": "#4c8dae"}},
+                        "data": [{
+                            "symbolRotate": 348.4654,
+                            "value": 2.0004,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 32.00537,
+                            "value": 0.56603885,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 336.62582,
+                            "value": 1.285496,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 321.90356,
+                            "value": 3.176728,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 304.8085,
+                            "value": 2.557655,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 329.15762,
+                            "value": 1.6189502,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 344.0546,
+                            "value": 0.2912044,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 322.7982,
+                            "value": 2.7620463,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 293.23352,
+                            "value": 2.154739,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 314.74982,
+                            "value": 1.6192899,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 337.95972,
+                            "value": 3.117852,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 280.4915,
+                            "value": 1.6475435,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 332.46802,
+                            "value": 2.1200943,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 27.093323,
+                            "value": 5.335485,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 359.25272,
+                            "value": 2.3001957,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 4.6241455,
+                            "value": 5.457765,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 352.18808,
+                            "value": 3.310725,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 358.36343,
+                            "value": 2.8011427,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 323.97263,
+                            "value": 3.128338,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {
+                            "symbolRotate": 318.8295,
+                            "value": 3.4938376,
+                            "symbol": "arrow",
+                            "symbolSize": "10"
+                        }, {"symbolRotate": 318.47778, "value": 2.5644493, "symbol": "arrow", "symbolSize": "10"}]
                     },
-                    data: [5, 20, 36, 10, 10, 20]
-                },{
-                    xAxisIndex: 1,
-                    name: '销量',
-                    type: 'line',
-                    smooth:true,   //关键点，为true是不支持虚线的，实线就用true
-                    itemStyle:{
-                        normal:{
-                            lineStyle:{
-                                width:2,
-                                type:'dotted'  //'dotted'虚线 'solid'实线
-                            }
-                        }
+                    {
+                        "name": "降水量",
+                        "type": "bar",
+                        "barWidth": 15,
+                        "yAxisIndex": 2,
+                        "itemStyle": {"normal": {"color": "#065279"}},
+                        "data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
                     },
-                    data: [5, 20, 36, 10, 10, 20]
-                }]
+                    {
+                        "name": "湿度",
+                        "type": "line",
+                        "yAxisIndex": 3,
+                        "itemStyle": {"normal": {"color": "#675bba"}},
+                        "data": [43.92, 43.96, 46.4, 41.94, 45.57, 48.08, 56.8, 58.38, 59.95, 59.04, 59.75, 59.15, 57.74, 53.54, 54.59, 49.16, 45.56, 42.42, 41.73, 39.36, 40.94],
+                        markLine: {
+                            symbolSize: 0,
+                            "itemStyle": {
+                                type:"line",
+                                "normal":{
+                                    lineStyle:{
+                                        "color": "#000",
+                                        "width": 2,
+                                        "type":"solid"
+                                    }
+
+                                }
+                            },
+                            data: [
+                                [
+                                    {xAxis: '20180330,01:30', yAxis: '0'},
+                                    {xAxis: '20180330,01:30', yAxis: '60'}
+                                ]
+                            ]
+                        }
+                    }
+                ]
             };
             chartUtil.init('chart',option);
         },
